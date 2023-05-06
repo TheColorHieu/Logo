ccb13d9b12f19c729c0b1a2788dbf048c28ecbbe
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-05-05 20:00:36 -0700

    questions are working!

--- index.js
+++ index.js
@@ -18,7 +18,76 @@ svgString += `${answers.shape}`;
 
 //here we are setting up the properties of the shapes and color 
 let shapeChoice;
-if (answers.shape === "Triangle"){
-    
+switch (answers.shape) {
+  case "Triangle":
+    shapeChoice = new Triangle();
+    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+    break;
+  case "Square":
+    shapeChoice = new Square();
+    svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+    break;
+  default:
+    shapeChoice = new Circle();
+    svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+    break;
 }
+ // <text> tag gives rise to text alignment, text-content/text-color taken in from user prompt and gives default font size of "40"
+ svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+ // Closing </g> tag
+ svgString += "</g>";
+ // Closing </svg> tag
+ svgString += "</svg>";
+
+ //this function takes in promptUser function along the svg string to be written in this file while letting user know if we are succesful 
+ fs.writeFile(fileName, svgString, (err) => {
+    err ? console.log(err) : console.log("Generated logo.svg");
+ });
 }
+//creating the function for asking the user questions 
+function promptUser(){
+    inquirer
+    .prompt([
+        //text prompt
+        {
+        type: "input",
+        message: "What text would you like your logo to display? (Enter up to three characters)",
+        name: "text",
+        },
+        //text color 
+        {
+            type: "input",
+            message: "Choose text color (Enter color eyword OR a hexadecimal number)",
+            name: "textColor",
+        },
+        //shape choice prompt 
+        {
+            type: "list",
+            message: "What shape would you like the logo to render?",
+            choices: [
+                "Triangle",
+                "Square",
+                "Circle"
+            ],
+            name: "shape",
+        },
+        //shape color 
+        {
+         type: "input",
+         message: "Choose shapes color (Enter color OR hexadecimal number)",
+         name: "shapBackgroundColor",
+        },
+    ])
+    .then((answers) => {
+        //catching the error for the text prompt if users is entering more less than 3 characters 
+        if(answers.text.length > 3) {
+            console.log("Must enter a value of no more than 3 characters");
+            promptUser();
+        } else{
+            //calling the write function to generate SVG file
+            writeToFile("logo.svg",answers);
+        }
+    });
+}
+
+promptUser();
